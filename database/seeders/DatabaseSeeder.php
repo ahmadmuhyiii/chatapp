37104a2d29e594e4dbbf7d204931539236bb8098
ahmadmuhyiii <ahmadmuhyi434@gmail.com>
2023-08-17 16:53:44 +0700

    aplikasi chat

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+
+    public function run()
+    {
+        $users = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('password'),
+            ],
+            [
+                'name' => 'User',
+                'email' => 'user@gmail.com',
+                'password' => bcrypt('password'),
+            ]
+        ];
+
+        User::insert($users);
+    }
+}
